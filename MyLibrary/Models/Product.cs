11898ba76b5a3a0b0f869962d8fe57dac84eb383
Omodademu Awoyelu <A00278997@mycambrian.ca>
2024-02-23 07:23:54 -0500

    Inside the class library project, created the Product, Category Models.

---
+++ MyLibrary/Models/Product.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace MyLibrary.Models
+{
+    public class Product
+    {
+       public int Id { get; set; }
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; } // Foreign key to Category
+        public Category Category { get; set; } // Navigation property
+    }
+}
