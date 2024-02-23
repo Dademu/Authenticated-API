373ec686353fbd8cf83d460898841cb1449287cd
Omodademu Awoyelu <A00278997@mycambrian.ca>
2024-02-23 16:44:40 -0500

    commit

--- MyApi/Controllers/ShoppingCartController.cs
+++ MyApi/Controllers/ShoppingCartController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using MyApi.Data;
 using MyLibrary.Models;
 using System.Collections.Generic;
 using System.Linq;
--- MyApi/Data/AppDbContext.cs
+++ MyApi/Data/AppDbContext.cs
@@ -1,34 +1,27 @@
 using System;
 using Microsoft.EntityFrameworkCore;
-using MyLibrary;
 using MyLibrary.Models;
 
-namespace YourNamespace.Data
+namespace MyApi.Data;
 {
     public class AppDbContext : DbContext
+{
+    // DbSet properties for application data
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+    // Override OnModelCreating to configure relationships, if needed
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // DbSet properties for application data
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
-
-        // Constructor to configure DbContext options
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
-
-        // Override OnModelCreating to configure relationships, if needed
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            optionsBuilder.UseSqlServer("your_connection_string_here"); // Example of configuring a connection string
-            // Configure relationships, if needed
-            modelBuilder.Entity<Product>()
-                .HasOne(p => p.Category)
-                .WithMany()
-                .HasForeignKey(p => p.CategoryId)
-                .IsRequired(false); // Example of configuring a relationship between Product and Category
-        }
+        optionsBuilder.UseSqlServer("your_connection_string_here"); // Example of configuring a connection string
+                                                                    // Configure relationships, if needed
+        modelBuilder.Entity<Product>()
+            .HasOne(p => p.Category)
+            .WithMany()
+            .HasForeignKey(p => p.CategoryId)
+            .IsRequired(false); // Example of configuring a relationship between Product and Category
     }
 }
-
-
+}
